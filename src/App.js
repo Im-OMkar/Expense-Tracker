1f5ee9521be3d9cbe78bd5838ec99d33b2558dd2
Omi <omkar.ingale@thoughtworks.com>
2022-04-07 10:07:35 +0530

    Added ExpenseDate component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Expense Tracker</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
