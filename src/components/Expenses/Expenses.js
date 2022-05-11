4ccf76c1f2d336fa4d326053fd1811810b851d0f
Omi <omkar.ingale@thoughtworks.com>
2022-05-11 09:06:46 +0530

    Added. ExpensesFilter Component : filter according to year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 function Expenses(props) {
+  const [filterYear, setFilterYear] = useState("");
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+    console.log(filterYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter value={filterYear} onFilterChange={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
