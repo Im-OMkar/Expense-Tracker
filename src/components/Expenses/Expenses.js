11b361dffd7198c4acfe0d699fa90dca60a8b059
Ishika Nagar <ishika.nagar@thoughtworks.com>
2022-08-26 18:09:21 +0530

    Added . extracted expense list from expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
@@ -15,29 +15,13 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filterYear;
     });
 
-    const displayExpense = () => {
-        return(
-            filteredExpenses.length === 0 ? <p> No Expenses Found</p> :
-                filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))
-        )
-    };
-
     return (
         <Card className="expenses">
             <ExpensesFilter
                 selected={filterYear}
                 onFilterChange={filterChangeHandler}
             />
-            {
-                displayExpense()
-            }
+            <ExpenseList items={filteredExpenses} />
         </Card>
     );
 }
