2a981fef0bf7f5f6ca1133069d14d1bcb8e87021
Ishika Nagar <ishika.nagar@thoughtworks.com>
2022-08-26 19:27:35 +0530

    Added conditional rendering for new expense button and form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-  const onSaveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData = {
-      ...enteredExpenseData,
-      id: Math.random.toString(),
-    };
-    props.onAddExpense(expenseData);
-  };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
-    </div>
-  );
+    const [show, setShow] = useState(false);
+
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random.toString(),
+        };
+        props.onAddExpense(expenseData);
+    };
+
+    const onClickButtonHandler = () => {
+        console.log("IShikaaaaaaaaaaa")
+        setShow(true);
+    };
+
+    return (
+        <div className="new-expense">
+            {!show && <button onClick={onClickButtonHandler}>Add new expense</button>}
+            {show && <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />}
+        </div>
+    );
 };
 
 export default NewExpense;
